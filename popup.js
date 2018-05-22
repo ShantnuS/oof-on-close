5024a3119480d49ffdee4c89fc5378ef70bd94dd
ShantnuS <shantnu@outlook.com>
2018-05-22 23:44:05 +0100

    fixed local storage bug

--- popup.js
+++ popup.js
@@ -2,8 +2,14 @@ var  bgp = chrome.extension.getBackgroundPage()
 
 document.addEventListener('DOMContentLoaded', function () {
 	var btn = document.getElementById("btnToggle");
-	btn.value = "Toggle: " + localStorage.play_audio.toUpperCase();
     btn.addEventListener('click', toggle);
+
+	if(localStorage.play_audio == undefined) {
+        localStorage.play_audio = "false";
+    }
+	
+	btn.value = "Toggle: " + localStorage.play_audio.toUpperCase();
+	
 });
 
 function toggle(){
