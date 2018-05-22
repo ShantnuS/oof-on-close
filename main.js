a2d988430a25abe3b87c0881dc800910fe23f42d
ShantnuS <shantnu@outlook.com>
2018-05-22 23:13:10 +0100

    official v1.1

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var audio;
 chrome.tabs.onRemoved.addListener(function(tabid, removed) {
 	if(JSON.parse(localStorage.play_audio)){
 		audio = new Audio('oof.mp3');
-		localStorage.playback_rate = 1;
-		audio.playbackRate = localStorage.playback_rate;
+		//localStorage.playback_rate = 1;
+		//audio.playbackRate = localStorage.playback_rate;
 		audio.play();
 	}
 })
